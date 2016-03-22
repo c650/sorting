72904bb98d1dceedc5b60ba2e45490f7ae679bc4
c650 <cbailey@palmertrinity.org>
2016-03-22 08:37:53 -0400

    fixing insertion sort yet again, add merge sort

--- C++/insertionsort.cpp
+++ C++/insertionsort.cpp
@@ -13,28 +13,41 @@ bool sorted(vector<char>& vec) {
 }
 void insertionsort(vector<char>& vec) {
 	int n = vec.size();
-
-	for (int j = 1; j < n; j++) {
+	/* 
+	* The first element in the array is treated as sorted,
+	* so we go on to the second one, vec[1], which is why
+	* the for loop starts at i = 1
+	*/
+	while (!sorted(vec)) {
 		for (int i = 1; i < n; i++) {
+			// if the element to the left bigger than the element we are looking at, vec[i]
+			// we need to put vec[i] where it belongs.
+			// and we do this by temporarily storing the value of vec[i] and then shifting
+			// all the elements of the array/vec that are bigger than vec[i] to the right
+			// until we arrive at vec[i]'s new, rightful place
 			if (vec[i-1] > vec[i]) {
 				int hold = vec[i]; // hold value
 
 				int pos = i; // track positive to insert `hold`
 				
-				while ( pos > 0) {
+				while (pos > 0 && vec[pos - 1] > hold) {
+					
 					vec[pos] = vec[pos - 1];
 					pos--;
-					
-					if (vec[pos + 1] <= hold) break;
-				}
 
+					// but, if our original vec[i] is bigger than
+					// the element we shifted in the last iteration,
+					// we can stop shifting
+				}
+				// now we can put the original vec[i] where it belongs by accessing
+				// the pos variable which holds the position we want to INSERT the
+				// original vec[i] into
 				vec[pos] = hold;
 			}
 		}
-		if (sorted(vec)) break;
 	}
 }
-
+// BELOW IS JUST INPUT, FEEL FREE TO OVERLOOK
 int main() {
 	cout << "give me a string" << endl;
 	string s; getline(cin, s);
